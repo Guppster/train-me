15e6ab1ea106d990d321745b6bf82aaf32f9ed31
Gurpreet Singh <gurpreet@mailfence.com>
2018-05-21 20:13:57 -0500

    Start the actons list

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,37 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main()
-=> runApp(new MyApp());
+void main() => runApp(new TrainMe());
 
-class MyApp extends StatelessWidget
-{
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context)
-    {
-        return new MaterialApp(
-            title: 'Flutter Demo',
-            theme: new ThemeData(
-                primarySwatch: Colors.orange,
-            ),
-            home: new MyHomePage(title: 'Train Me'),
-        );
-    }
+class TrainMe extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Train Me',
+      theme: new ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Actions'),
+        ),
+        body: new Center(
+          child: new ActionsList(),
+        ),
+      ),
+    );
+  }
 }
 
-class MyHomePage extends StatefulWidget
-{
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    final String title;
-
-    @override
-    _MyHomePageState createState()
-    => new _MyHomePageState();
+class ActionsList extends StatefulWidget {
+  @override
+  ActionsListState createState() => new ActionsListState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-{
-    int _counter = 0;
-
-    void _incrementCounter()
-    {
-        setState(()
-        {
-            _counter++;
-        });
-    }
-
-    @override
-    Widget build(BuildContext context)
-    {
-        return new Scaffold(
-            appBar: new AppBar(title: new Text(widget.title)),
-
-            body: new Center(
-                child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                        new Text('You have pushed the button this many times:'),
-                        new Text('$_counter', style: Theme.of(context).textTheme.display1),
-                    ])),
-
-            floatingActionButton: new FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: new Icon(Icons.add),
-            ),
-        );
-    }
+class ActionsListState extends State<ActionsList> {
+  @override
+  Widget build(BuildContext context) {
+      return new Text(new WordPair.random().asPascalCase);
+  }
 }
